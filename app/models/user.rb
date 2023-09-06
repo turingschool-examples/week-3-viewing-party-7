af069a8a1c0a818f8ec5b10a38b20b65d7c01019
Anna Wiley <awiley33@gmail.com>
2023-09-05 21:34:23 -0500

    Add functionality for user story 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User <ApplicationRecord 
-  validates_presence_of :email, :name 
+  validates_presence_of :email, :name, :password_digest
   validates_uniqueness_of :email
+
+  has_secure_password
 end 
