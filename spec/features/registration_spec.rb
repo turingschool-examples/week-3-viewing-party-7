679565dcba374bed8c1ddb9fe14d43d105fe9978
Anna Wiley <awiley33@gmail.com>
2023-09-05 21:34:10 -0500

    Add testing for user story 1

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "User Registration" do
 
     fill_in :user_name, with: 'User One'
     fill_in :user_email, with:'user1@example.com'
+    fill_in :user_password, with: 'password123'
     click_button 'Create New User'
 
     expect(current_path).to eq(user_path(User.last.id))
@@ -13,12 +14,14 @@ RSpec.describe "User Registration" do
   end 
 
   it 'does not create a user if email isnt unique' do 
-    User.create(name: 'User One', email: 'notunique@example.com')
+    User.create(name: 'User One', email: 'notunique@example.com', password: "password123", password_confirmation: "password123")
 
     visit register_path
     
     fill_in :user_name, with: 'User Two'
     fill_in :user_email, with:'notunique@example.com'
+    fill_in :user_password, with: 'password123'
+    fill_in :user_password_confirmation, with: 'password123'
     click_button 'Create New User'
 
     expect(current_path).to eq(register_path)
