1da59fa9fcbd03e25bfca603da95b71ab1318d03
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-10-23 13:30:43 -0600

    test: Start updates to add authenticaition testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User <ApplicationRecord 
-  validates_presence_of :email, :name 
-  validates_uniqueness_of :email
+  validates_presence_of :name
+  validates_presence_of :email, uniqueness: true, presence: true
+
+
   has_many :viewing_parties
+  has_secure_password
 end 
