96bc1db5bd0343ed754cc23399db64952e341622
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-10-23 20:24:22 -0600

    feat: Complete User Story 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User <ApplicationRecord 
   validates_presence_of :name
-  validates_presence_of :email, uniqueness: true, presence: true
+  validates_presence_of :email 
+  validates_uniqueness_of :email
   validates_presence_of :password_digest
-
 
   has_many :viewing_parties
   has_secure_password
--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -16,15 +16,32 @@ RSpec.describe "User Registration" do
   end 
 
   it 'does not create a user if email isnt unique' do 
-    User.create(name: 'User One', email: 'notunique@example.com')
+    User.create(name: 'User One', email: 'user1@example.com', password: 'password123', password_confirmation: 'password123')
 
     visit register_path
     
     fill_in :user_name, with: 'User Two'
-    fill_in :user_email, with:'notunique@example.com'
+    fill_in :user_email, with:'user1@example.com'
+    fill_in :user_password, with:'password456'
+    fill_in :user_password_confirmation, with:'password456'
+
     click_button 'Create New User'
 
     expect(current_path).to eq(register_path)
     expect(page).to have_content("Email has already been taken")
+  end
+
+  it 'does not create a user if passwords do not match' do
+    visit register_path
+  
+    fill_in :user_name, with: 'User One'
+    fill_in :user_email, with: 'user1@example.com'
+    fill_in :user_password, with: 'password123'
+    fill_in :user_password_confirmation, with: 'password345'
+  
+    click_button 'Create New User'
+  
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Password confirmation doesn't match Password")
   end
 end
