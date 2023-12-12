d4df81733e37a3135c5967bfece3ee96b1f205ce
Cory Powell <coryrpowell@gmail.com>
2023-12-11 21:53:06 -0500

    added testing for US1 and migrations for a new password_digest column

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -6,22 +6,47 @@ RSpec.describe "User Registration" do
 
     fill_in :user_name, with: 'User One'
     fill_in :user_email, with:'user1@example.com'
+    fill_in :user_password, with:'test'
+    fill_in :user_password_confirmation, with:'test'
     click_button 'Create New User'
+    # require 'pry';binding.pry
 
     expect(current_path).to eq(user_path(User.last.id))
-    expect(page).to have_content("User One's Dashboard")
+    expect(page).to have_content("user one's Dashboard")
   end 
 
   it 'does not create a user if email isnt unique' do 
-    User.create(name: 'User One', email: 'notunique@example.com')
+    User.create(name: 'User One', email: 'notunique@example.com', password: "test", password_confirmation: "test")
 
     visit register_path
     
     fill_in :user_name, with: 'User Two'
     fill_in :user_email, with:'notunique@example.com'
+    fill_in :user_password, with:'test'
+    fill_in :user_password_confirmation, with:'test'
     click_button 'Create New User'
 
     expect(current_path).to eq(register_path)
     expect(page).to have_content("Email has already been taken")
+  end
+
+  it "does not create a user if the passwords don't match" do
+#   As a visitor 
+# When I visit `/register`
+# I see a form to fill in my name, email, password, and password confirmation.
+# When I fill in that form with my name, email, and matching passwords,
+# I'm taken to my dashboard page `/users/:id`
+    User.create(name: 'User One', email: 'otunique@example.com', password: "password", password_confirmation: "antipassword")
+    visit register_path
+
+    fill_in :user_name, with: 'User Two'
+    fill_in :user_email, with: 'user2@example.com'
+    fill_in :user_password, with: 'password'
+    fill_in :user_password_confirmation, with: 'antipassword'
+
+    click_on "Create New User"
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Password confirmation doesn't match Password")
   end
 end
